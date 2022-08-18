fa5b7f9193a9e02832c925c6fdf17a9a2d40333c
Jhansi Lakshmi <84505647+GondiJhansi@users.noreply.github.com>
2022-08-18 09:39:52 +0530

    Add files via upload

---
+++ Deleting_Dict.py
@@ -0,0 +1,18 @@
+dic={100:'jhansi',200:'bhuvan',300:'muneer'}
+print(dic)
+del dic[100] #*********************#
+print(dic)
+dic.clear() #**********************#
+print(dic)
+dic[998]='padma'
+dic[999]='sankar'
+print(dic)
+dic.clear()
+print(dic)
+dic[999,998]='padma','sankar'
+print(dic)
+
+
+l=['jhansi','bhuvan']
+d={100:l}
+print(d)
